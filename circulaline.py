ae326cca5aabddcd00396d064616b5d460607732
deprekate <deprekate@gmail.com>
2019-09-25 20:51:31 -0700

    Initial commit

---
+++ circulaline.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import csv
+
+old_chars = "ACGTacgt"
+replace_chars = "TGCAtgca"
+
+if len(sys.argv) < 3:
+	raise ValueError("usage: python circulaline.py FOLDER LOCATION_FILE")
+
+folder_path = sys.argv[1]
+nucleotide_dict = dict()
+
+for filename in os.listdir(folder_path):
+	if filename.endswith(tuple(['fasta', 'fna'])):
+		nucleotides = ''
+		with open(os.path.join(folder_path, filename)) as file_contents:
+			for line in file_contents:
+				if line.startswith('>'):
+					header = line[1:].rstrip()
+				else:
+					nucleotides += line.rstrip()
+		nucleotide_dict[header] = nucleotides
+
+reader = csv.reader(open(sys.argv[2], 'r'), delimiter='\t')
+coords = {}
+for row in reader:
+	k, v = row
+	coords[k] = int(v)
+
+for name in nucleotide_dict:
+	start = coords[name]
+	nucleotides = nucleotide_dict[name]
+	if start < 0:
+		tab = str.maketrans(old_chars,replace_chars)
+		nucleotides = nucleotides.translate(tab)[::-1]
+		start += 1
+
+	print(nucleotides[start:] + nucleotides[:start])
+
---
+++ get_location.py
@@ -0,0 +1,15 @@
+import sys
+
+for line in sys.stdin:
+	fields = line.split()
+	if fields[8] < fields[9]: 
+		sys.stdout.write(fields[0])
+		sys.stdout.write("\t")
+		sys.stdout.write(fields[6])
+		sys.stdout.write("\n")
+	else:
+		sys.stdout.write(fields[0])
+		sys.stdout.write("\t")
+		sys.stdout.write("-")
+		sys.stdout.write(fields[7])
+		sys.stdout.write("\n")
